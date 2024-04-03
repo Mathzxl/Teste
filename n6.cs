7ca294a1297fe9c01aeb458d132c6a59c246f843
Mathzxl <162761845+Mathzxl@users.noreply.github.com>
2024-04-02 21:21:29 -0300

    Add files via upload

---
+++ n6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Desenvolva um algoritmo que imprima os n primeiros elementos da sequência de Fibonacci.
+Ex.: Se n = 10, imprimir os 10 primeiros números da sequência de Fibonacci (Obs: Pesquise na
+internet como a sequência de Fibonacci é calculada). */
+            int n;
+            Console.WriteLine("De um valor qualquer");
+            n = int.Parse(Console.ReadLine());
+            int a = 1;
+            int b = 2;
+            int contador = 0;
+            int c = 0;
+            Console.WriteLine("Os primeiro " + n + " números da sequência de Fibonacci são:");
+            while (contador < n)
+            {
+                Console.WriteLine(c);
+                c = a + b;
+                a = b;
+                b = c;
+                contador++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ n7.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i = 1;
+            double soma = 0;
+            Console.WriteLine("Digite um valor inteiro positivo:");
+            n = int.Parse(Console.ReadLine());
+            while (i <= n)
+            {
+                soma += (double)1 / (i * (i + 1));
+                i++;
+            }
+            Console.WriteLine("O valor de S é: " + soma);
+            Console.ReadLine();
+        }
+    }
+}
